bc455996880d63cb4824da299593bdf495d845da
SayanoAI <28544690+SayanoAI@users.noreply.github.com>
2023-09-02 22:34:54 -0400

    fixed tensorboard display

--- pages/4_Tensorboard.py
+++ pages/4_Tensorboard.py
@@ -1,5 +1,6 @@
 from streamlit_tensorboard import st_tensorboard
 import streamlit as st
+st.set_page_config(layout="wide")
 
-logdir=st.text_input("Path to Logs",value="./logs")
+logdir=st.text_input("Path to Logs",value="/logs")
 if logdir: st_tensorboard(logdir=logdir, port=6006)
