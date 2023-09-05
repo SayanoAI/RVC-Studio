3877e5b2fa219b1326b25139baf9df0dda17e56b
SayanoAI <28544690+SayanoAI@users.noreply.github.com>
2023-09-04 22:23:05 -0400

    manually start tensorboard button

--- pages/4_Tensorboard.py
+++ pages/4_Tensorboard.py
@@ -1,12 +1,33 @@
 import os
+import subprocess
 import sys
+import psutil
 from streamlit_tensorboard import st_tensorboard
+from web_utils import MENU_ITEMS
 import streamlit as st
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",menu_items=MENU_ITEMS)
+
+from webui_utils import render_subprocess_list
+from web_utils.contexts import SessionStateContext
+
 
 CWD = os.getcwd()
 if CWD not in sys.path:
     sys.path.append(CWD)
     
-logdir=st.text_input("Path to Logs",value=os.path.join(CWD,"logs"))
-if logdir: st_tensorboard(logdir=logdir, port=6006)
+def start_tensorboard(logdir):
+    cmd = f"tensorboard --logdir={logdir}"
+    p = subprocess.Popen(cmd, shell=True, cwd=CWD,stdout=subprocess.PIPE)
+    return p
+
+with SessionStateContext("tensorboard") as state:
+    state.logdir=st.text_input("Path to Logs",value=state.logdir if state.logdir else os.path.join(CWD,"logs"))
+    if state.logdir:
+        st_tensorboard(logdir=state.logdir, port=6006)
+
+        if not state.process:
+            if not any(["tensorboard" in p.name() for p in psutil.Process(os.getpid()).children(recursive=True)]):
+                if st.button("Start Tensorboard"):
+                    state.process = start_tensorboard(state.logdir)
+
+    render_subprocess_list()
