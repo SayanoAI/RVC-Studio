39b272557a919eb63a248a47fb5182bc43ca45c8
SayanoAI <28544690+SayanoAI@users.noreply.github.com>
2023-09-22 11:31:24 -0400

    gpus default value

--- pages/2_Train.py
+++ pages/2_Train.py
@@ -246,7 +246,7 @@ def init_training_state():
         if_save_latest=True,
         pretrained_G=None,
         pretrained_D=None,
-        gpus=[0],
+        gpus=[],
         if_cache_gpu=False,
         if_save_every_weights=False,
         version="v2",
@@ -299,7 +299,7 @@ if __name__=="__main__":
         with st.form(i18n("training.train_model.form")):  #def train_model(exp_dir,if_f0,spk_id,version,sr,gpus,batch_size,total_epoch,save_epoch,pretrained_G,pretrained_D,if_save_latest,if_cache_gpu,if_save_every_weights):
             st.subheader(i18n("training.train_model.title"))
             st.write(i18n("training.train_model.text"))
-            state.gpus=st.multiselect(i18n("training.gpus"),options=[str(i) for i in range(torch.cuda.device_count())])
+            state.gpus=st.multiselect(i18n("training.gpus"),options=[str(i) for i in range(torch.cuda.device_count())],default=state.gpus)
             col1,col2,col3=st.columns(3)
             state.batch_size=col1.slider(i18n("training.batch_size"),min_value=1,max_value=100,step=1,value=state.batch_size)
             state.total_epoch=col2.slider(i18n("training.total_epoch"),min_value=0,max_value=1000,step=10,value=state.total_epoch)
